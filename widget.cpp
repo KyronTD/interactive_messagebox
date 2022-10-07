201ed9e8284b48575ed9d9dfe3cb3d52e60c5eb6
kierantoangutu <kieran.tee@gmail.com>
2022-10-07 18:06:14 +1300

    Second Commit

--- widget.cpp
+++ widget.cpp
@@ -1,6 +1,8 @@
 #include "widget.h"
 #include "./ui_widget.h"
 
+#include <QDebug>
+
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
@@ -16,6 +18,18 @@ Widget::~Widget()
 
 void Widget::on_pushButton_clicked()
 {
+    //Display Text Edit
+    qDebug() << "Text Edit 1 output =" << ui->textEdit->toPlainText();
+}
+
+
+void Widget::on_pushButton_2_clicked()
+{
+    //Display top four lines of edited text
+    qDebug() << "Line Edit 1 output =" << ui->lineEdit->text();
+    qDebug() << "Line Edit 2 output =" << ui->lineEdit_2->text();
+    qDebug() << "Line Edit 3 output =" << ui->lineEdit_3->text();
+    qDebug() << "Line Edit 4 output =" << ui->lineEdit_4->text();
 
 }
 
